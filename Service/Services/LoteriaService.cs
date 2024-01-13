1b7b20e97107bf415994b08acec9088f0821990f
Kako Ferrare <kakoferrare87@gmail.com>
2024-01-13 19:37:08 -0300

    refactor: remoção de escopo para debug

--- Service/Services/LoteriaService.cs
+++ Service/Services/LoteriaService.cs
@@ -9,24 +9,20 @@ namespace Service.Services
     public class LoteriaService<T> : ILoteriaService<T> where T : LoteriaDocument
     {
         private readonly ILoteriaRepositoryFactory<T, ILoteriaRepository<T>> _repositoryFactory;
-        private readonly IMegaSenaRepository _repo;
 
-        public LoteriaService(ILoteriaRepositoryFactory<T, ILoteriaRepository<T>> repositoryFactory, IMegaSenaRepository repository)
+        public LoteriaService(ILoteriaRepositoryFactory<T, ILoteriaRepository<T>> repositoryFactory)
         {
             _repositoryFactory = repositoryFactory;
-            _repo = repository;
         }
 
         public async Task<IEnumerable<T>> GetAsync(eLoteria loteria)
         {
-            var teste = await _repo.GetLastAsync();
             var repository = _repositoryFactory.FactoryRepository(loteria);
             return (IEnumerable<T>)await repository.GetAsync();
         }
 
         public async Task<T> GetLastAsync(eLoteria loteria)
         {
-            var teste = await _repo.GetLastAsync();
             var repository = _repositoryFactory.FactoryRepository(loteria);
             return (T)await repository.GetLastAsync();
         }
