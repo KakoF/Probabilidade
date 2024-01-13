45db93ed273f5b4c1685da4c3c252d8f94d8f170
Kako Ferrare <kakoferrare87@gmail.com>
2024-01-13 16:56:33 -0300

    feat: correção na injeção da infrastructure

--- Infrastructure/RegisterService.cs
+++ Infrastructure/RegisterService.cs
@@ -12,7 +12,7 @@ namespace Infrastructure
         public static void ConfigureInfraStructure(this IServiceCollection services)
         {
             services.AddScoped(typeof(IMongoRepository<>), typeof(MongoRepository<>));
-            services.AddScoped<ILoteriaRepository<DuplaSenaDocument>, DuplaSenaRepository>();
+            /*services.AddScoped<ILoteriaRepository<DuplaSenaDocument>, DuplaSenaRepository>();
             services.AddScoped<ILoteriaRepository<FederalDocument>, FederalRepository>();
             services.AddScoped<ILoteriaRepository<LotoFacilDocument>, LotoFacilRepository>();
             services.AddScoped<ILoteriaRepository<LotoManiaDocument>, LotoManiaRepository>();
@@ -20,7 +20,19 @@ namespace Infrastructure
             services.AddScoped<ILoteriaRepository<MegaSenaDocument>, MegaSenaRepository>();
             services.AddScoped<ILoteriaRepository<QuinaDocument>, QuinaRepository>();
             services.AddScoped<ILoteriaRepository<SuperSeteDocument>, SuperSeteRepository>();
-            services.AddScoped<ILoteriaRepository<TimeManiaDocument>, TimeManiaRepository>();
+            services.AddScoped<ILoteriaRepository<TimeManiaDocument>, TimeManiaRepository>();*/
+
+
+            services.AddScoped<IDuplaSenaRepository, DuplaSenaRepository>();
+            services.AddScoped<IFederalRepository, FederalRepository>();
+            services.AddScoped<ILotoFacilRepository, LotoFacilRepository>();
+            services.AddScoped<ILotoManiaRepository, LotoManiaRepository>();
+            services.AddScoped<IMaisMilionariaRepository, MaisMilionariaRepository>();
+            services.AddScoped<IMegaSenaRepository, MegaSenaRepository>();
+            services.AddScoped<IQuinaRepository, QuinaRepository>();
+            services.AddScoped<ISuperSeteRepository, SuperSeteRepository>();
+            services.AddScoped<ITimeManiaRepository, TimeManiaRepository>();
+
             services.AddScoped(typeof(ILoteriaRepositoryFactory<LoteriaDocument>), typeof(LoteriaRepositoryFactory<LoteriaDocument>));
         }
     }
