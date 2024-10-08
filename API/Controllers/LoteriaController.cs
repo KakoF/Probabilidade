281dc4f552b7c80fccc1c720802f5136d1c1cd3a
Kako Ferrare <kakoferrare87@gmail.com>
2024-10-07 23:19:36 -0300

    feat: add endpoint erro

--- API/Controllers/LoteriaController.cs
+++ API/Controllers/LoteriaController.cs
@@ -1,6 +1,7 @@
 using Domain.Enums;
 using Domain.Records;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Text.RegularExpressions;
 
 namespace API.Controllers
@@ -18,5 +19,20 @@ namespace API.Controllers
 	            .Select(v => new Loteria(v.ToString(), Regex.Replace(v.ToString(), "(\\B[A-Z])", " $1")))
 	            .ToList();
         }
-    }
+
+
+		[HttpGet("Random")]
+		public IActionResult GetRandom()
+		{
+			Random random = new Random();
+			int randomNumber = random.Next(0, 101);
+			if (randomNumber >= 80)
+				return BadRequest();
+
+			return Ok(Enum.GetValues(typeof(eLoteria))
+				.Cast<eLoteria>()
+				.Select(v => new Loteria(v.ToString(), Regex.Replace(v.ToString(), "(\\B[A-Z])", " $1")))
+				.ToList());
+		}
+	}
 }
